70e0ea6add87682a9e47c038f1b9417636ec91ad
nebojsa.lalic <nebojsa.lalic@devtechgroup.com>
2017-04-04 16:21:13 +0200

    dependency injection and instatiate class changes - UPDATE

--- app/NL/bootstrap/bootstrap.php
+++ app/NL/bootstrap/bootstrap.php
@@ -2,6 +2,8 @@
 
 namespace app\NL\bootstrap;
 
+use app\NL\database\MysqlAdapter;
+use app\NL\database\MongoAdapter;
 use Klein\Klein;
 use Smarty;
 use app\NL\database\Database;
@@ -12,6 +14,8 @@ require_once __DIR__ . '\..\database\config.php';
 
 $klein = new Klein();
 $smarty = new Smarty();
-$database = new Database();
+$mysqlAdapter = new MysqlAdapter();
+$mongodbAdapter = new MongoAdapter();
+$database = new Database($mysqlAdapter, $mongodbAdapter);
 $validation = new Validation($database);
 $employeeService = new EmployeeService($validation, $database);
